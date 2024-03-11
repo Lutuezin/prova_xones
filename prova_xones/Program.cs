cc226f9b1f4d1b805043dacd876d34a6c99abce8
ALUNO <ALUNO@LABFPC25>
2024-03-11 08:43:05 -0300

    Adicionar arquivos de projeto.

---
+++ prova_xones/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova_xones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, p, div;
+            h = double.Parse(Console.ReadLine());
+            p = double.Parse(Console.ReadLine());
+            div = h / p;
+            Console.WriteLine(div) ;
+            Console.ReadKey();
+            
+
+        }
+    }
+}
